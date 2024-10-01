0154e24e53f1b7087859311e58de7733a9356dfd
Krishna K <krish1826s@gmail.com>
2024-10-01 10:00:51 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int[][] mat=new int[n][n]; 
+      
+      magicMatrix(mat,n);
+  }
+  static void magicMatrix(int[][] mat,int n){
+    int pi=0;
+    int pj=n/2;
+    int pl=0;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(mat[pi][pj]==0)
+        mat[pi][pj]=++pl;
+        if(pi==0&&pj==n-1){
+          pi=n-1;
+          pj=0;
+        }
+        else if(pi==0){
+          pi=n-1;
+          pj+=1;
+        }
+        else if(pj==n-1){
+          pj=0;
+          pi-=1;
+        }
+        else
+        {
+        pi-=1;
+        pj+=1;
+      }
+        print(mat,n);
+      }
+    }
+    
+  }
+  static void print(int[][] mat,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          System.out.print(mat[i][j]+" ");
+        }
+        System.out.println();
+      }
+      System.out.println();
+      System.out.println();
+  }
+}
