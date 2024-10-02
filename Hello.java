fe9ac150f3eb383b7198af07b80031f2efe2113d
Krishna K <krish1826s@gmail.com>
2024-10-02 19:46:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@
 // Hint
 // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
-// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn]. 
 
  
 
